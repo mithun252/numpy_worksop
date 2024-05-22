a2ed3a32704c2be159d897b6fae91f7392127c61
mithun252 <168530034+mithun252@users.noreply.github.com>
2024-05-22 10:08:40 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,9 +1,7 @@
-def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers."
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-number = int(input("Enter a number"))
-print(f"The factorial of {number} is {factorial(number)}")
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+num=int(input("Enter the number"))
+print("The factorial of", num, "is", factorial(num))
