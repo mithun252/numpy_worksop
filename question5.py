29ba844853db63bcf75c48ab69c052ee0a88db9b
mithun252 <168530034+mithun252@users.noreply.github.com>
2024-05-22 10:20:07 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,10 +1,1 @@
-def prime(n):
-    n=int(input("Enter the number"))
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i *
+
