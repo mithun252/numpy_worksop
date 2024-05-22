e78ec51b8059f144e298f690975d898ffbd4f020
mithun252 <168530034+mithun252@users.noreply.github.com>
2024-05-22 10:13:13 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,1 +1,10 @@
-#write a program to find if the given number is prime no or not
+def prime(n):
+    n=int(input("Enter the number"))
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i *
